25113e751894d2b6a5d4906d3d229fae23f12235
hayatul-islam <hayatulemon340@gmail.com>
2022-08-03 16:05:18 +0600

    dynamic route

---
+++ basic/hello-world/pages/product/index.js
@@ -0,0 +1,11 @@
+function Product() {
+  return (
+    <>
+      <h1>Product page</h1>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </>
+  );
+}
+export default Product;
