a633a78619084fa93751a6c85eca61c292d91362
hayatul-islam <hayatulemon340@gmail.com>
2022-08-04 21:29:03 +0600

    button route

--- basic/basic-part/pages/index.js
+++ basic/basic-part/pages/index.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("order");
+    // router.push("/product");
+    router.replace("/product");
+  };
   return (
     <div>
       <h1>Hello world</h1>
       <Link href="/blog">blog</Link>
       <Link href="/product">Products</Link>
+      <button onClick={handleClick}>Place order</button>
     </div>
   );
 }
