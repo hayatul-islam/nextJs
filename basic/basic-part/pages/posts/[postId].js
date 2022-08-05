0d8690cb076577f5ebcc658b8854c8ed6cbf65c1
hayatul-islam <hayatulemon340@gmail.com>
2022-08-05 23:02:51 +0600

    fallback true

--- basic/basic-part/pages/posts/[postId].js
+++ basic/basic-part/pages/posts/[postId].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
 export default function Post({ post }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <h4>{post.title}</h4>
@@ -9,7 +14,7 @@ export default function Post({ post }) {
 export async function getStaticPaths() {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const data = await response.json();
-  const paths = data.map((post) => {
+  const paths = data.slice(0, 3).map((post) => {
     return {
       params: {
         postId: `${post.id}`,
@@ -18,7 +23,8 @@ export async function getStaticPaths() {
   });
   return {
     paths,
-    fallback: false,
+    // fallback: false,
+    fallback: true,
   };
 }
 
@@ -28,6 +34,12 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
   const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
