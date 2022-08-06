53f56943c8a4d1fe717d076ef042ae3ebe42e37b
hayatul-islam <hayatulemon340@gmail.com>
2022-08-06 12:53:59 +0600

    server side dynamic parameter

---
+++ basic/basic-part/pages/news/[category].js
@@ -0,0 +1,33 @@
+export default function ArticleListCategory({ articles, category }) {
+  //   console.log(articles);
+  return (
+    <>
+      <h1>Showing news for category {category}</h1>
+      {articles?.map((article) => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { category } = params;
+  const response = await fetch(
+    `http://localhost:4000/news?category=${category}`
+  );
+  const data = await response.json();
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
+}
