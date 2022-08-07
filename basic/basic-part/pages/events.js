ff31cde652be72adb5b8007ae323071a0457696e
hayatul-islam <hayatulemon340@gmail.com>
2022-08-07 13:14:34 +0600

    clients side data fetching and shalow routing

---
+++ basic/basic-part/pages/events.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function EventList({ eventList }) {
+  const [events, setEvents] = useState(eventList);
+  const router = useRouter();
+  const fetchSportsEvents = async () => {
+    const response = await fetch(
+      "http://localhost:4000/events?category=sports"
+    );
+    const data = await response.json();
+    setEvents(data);
+    router.push("/events?category=sports", undefined, { shallow: true });
+  };
+  return (
+    <>
+      <h2>List of events</h2>
+      <button onClick={fetchSportsEvents}>Sports Events</button>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h3>
+            {event.title} | {event.category}
+          </h3>
+          <p>{event.description}</p>
+          <hr />
+        </div>
+      ))}
+    </>
+  );
+}
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { category } = query;
+  const queryString = category ? "category=sports" : "";
+  const response = await fetch(`http://localhost:4000/events?${queryString}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      eventList: data,
+    },
+  };
+}
