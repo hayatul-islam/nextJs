c364cdb80bdfc37444828e8f62606f8404d221ce
hayatul-islam <hayatulemon340@gmail.com>
2022-08-09 11:11:40 +0600

    new next app and api route

--- basic/basic-part/pages/events.js
+++ basic/basic-part/pages/events.js
@@ -10,6 +10,7 @@ export default function EventList({ eventList }) {
     );
     const data = await response.json();
     setEvents(data);
+    // router.push("/events?category=sports", undefined, { shallow: true });
     router.push("/events?category=sports", undefined, { shallow: true });
   };
   return (
@@ -28,6 +29,19 @@ export default function EventList({ eventList }) {
     </>
   );
 }
+// export async function getServerSideProps(context) {
+//   const { query } = context;
+//   const { category } = query;
+//   const queryString = category ? "category=sports" : "";
+//   const response = await fetch(`http://localhost:4000/events?${queryString}`);
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       eventList: data,
+//     },
+//   };
+// }
 export async function getServerSideProps(context) {
   const { query } = context;
   const { category } = query;
