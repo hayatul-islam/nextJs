5f8c0092ae9d0415853e829b7626814393e13df4
hayatul-islam <hayatulemon340@gmail.com>
2022-08-09 19:59:12 +0600

    post method

--- basic/next-api/pages/comments/index.js
+++ basic/next-api/pages/comments/index.js
@@ -1,20 +1,44 @@
-import { useSatate } from "react";
+import { useState } from "react";
 import { comments } from "../../data/comments";
 export default function CommentsPage() {
   const fetchComments = async () => {
-    const [comments, setComments] = useSatate();
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
     const response = await fetch("api/comments");
     const data = await response.json();
     setComments(data);
   };
+  const submitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <>
-      <button onClick={fetchComments}>Load comment</button>
-      {comments.map((comment) => (
-        <div key={comment.id}>
-          {comment.id} | {comment.text}
-        </div>
-      ))}
+      <div>
+        <input
+          type="text"
+          // value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button onClick={submitComment}>Submit comment</button>
+      </div>
+      <hr />
+      <button onClick={fetchComments}>Load comments</button>
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            {comment.id}. {comment.text}
+            {/* <button onClick={() => deleteComment(comment.id)}>Delete</button> */}
+          </div>
+        );
+      })}
     </>
   );
 }
